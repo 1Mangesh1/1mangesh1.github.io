af7b80b25ba6353c011803f0addb51f1bb732a19
Mangesh Bide <mangeshbide123@gmail.com>
2026-05-12 16:36:28 +0530

    asdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,9 @@ export default defineConfig({
   ],
   site: "https://mangeshbide.tech",
   base: "/",
+  redirects: {
+    "/dead": "https://mangeshbide-job-researcher.hf.space",
+  },
   build: {
     assets: "assets",
   },
